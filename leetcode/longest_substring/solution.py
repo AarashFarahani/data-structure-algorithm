60a7c85ae50f299769d5138545bd78459c2e3ef5
aarashfarahani <arashfarahani@hotmail.com>
2019-12-17 16:27:06 +0330

    longest substring has been resolved

--- leetcode/longest_substring/solution.py
+++ leetcode/longest_substring/solution.py
@@ -1,3 +1,33 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        
+        res = start = 0
+        dic = {}
+
+        for i, item in enumerate(s):
+            if item in dic: 
+                start = max(dic[item], start)
+            else:
+                dic[item] = start
+
+            res = max(res, i - start + 1)
+            dic[item] = i + 1
+
+        return res
+
+result = Solution().lengthOfLongestSubstring("abcabcbb")
+print(result)
+
+result = Solution().lengthOfLongestSubstring("bbbbb")
+print(result)
+
+result = Solution().lengthOfLongestSubstring("pwwkew")
+print(result)
+
+result = Solution().lengthOfLongestSubstring("aab")
+print(result)
+
+result = Solution().lengthOfLongestSubstring("dvdf")
+print(result)
+
+result = Solution().lengthOfLongestSubstring("dvdfafbcrtgszm")
+print(result)
